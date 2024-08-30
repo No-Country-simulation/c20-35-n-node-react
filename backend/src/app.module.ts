7ca6cb78b25483a130246a052b8559aa73fdf35f
Carlos Ochoa <uligibson@hotmail.com>
2024-08-30 17:46:07 -0600

    feature(backend): add nestjs starter kit

---
+++ backend/src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config'
+import { BreedsModule } from './breeds/breeds.module';
+import { CatsModule } from './cats/cats.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number.parseInt(<string>process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: (process.env.MODE === 'development'),
+      logging: true,
+      ssl: true,
+    }),
+    CatsModule,
+    BreedsModule,
+    UsersModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
