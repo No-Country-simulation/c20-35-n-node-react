29e566456004aa38921e3abbd72def373f80329b
Claus Castillo <xclauscastillox@gmail.com>
2024-09-19 16:08:13 -0400

    quick fix in modal layout

--- frontend/src/components/common/Modal.tsx
+++ frontend/src/components/common/Modal.tsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import { motion } from 'framer-motion'
-import { X, ArrowLeft } from 'lucide-react'
+import React from 'react';
+import { motion } from 'framer-motion';
+import { X, ArrowLeft } from 'lucide-react';
 
-export default function Modal({ children, setIsModalOpen, goBack }: {
-  children: React.ReactNode,
-  setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>,
-  goBack?: () => void
+export default function Modal({
+  children,
+  setIsModalOpen,
+  goBack,
+}: {
+  children: React.ReactNode;
+  setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  goBack?: () => void;
 }) {
   return (
     <motion.div
-      className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center"
+      className='fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center '
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
     >
       <motion.div
-        className="bg-card-bg p-6 rounded-xl lg:w-[650px] lg:min-h-[350px] overflow-y-auto relative"
+        className='bg-card-bg p-6 rounded-xl max-w-[90%] lg:w-[650px] h-[80%] overflow-y-auto relative pt-10'
         initial={{ scale: 0.9, opacity: 0 }}
         animate={{ scale: 1, opacity: 1 }}
         exit={{ scale: 0.9, opacity: 0 }}
@@ -23,20 +27,19 @@ export default function Modal({ children, setIsModalOpen, goBack }: {
         {children}
         {goBack && (
           <button
-            className="absolute top-3 left-3 text-blue-500 hover:text-blue-600 transition-colors duration-200"
+            className='absolute top-3 left-3 text-blue-500 hover:text-blue-600 transition-colors duration-200'
             onClick={goBack}
           >
-            <ArrowLeft className="h-6 w-6" />
+            <ArrowLeft className='h-6 w-6' />
           </button>
         )}
         <button
-          className="absolute top-3 right-3 text-blue-500 hover:text-red hover:text-red-600 transition-colors duration-200"
+          className='absolute top-3 right-3 text-blue-500 hover:text-red hover:text-red-600 transition-colors duration-200'
           onClick={() => setIsModalOpen(false)}
         >
-          <X className="h-6 w-6" />
+          <X className='h-6 w-6' />
         </button>
-
       </motion.div>
     </motion.div>
-  )
+  );
 }
